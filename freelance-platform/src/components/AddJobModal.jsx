9af2c34feb159a972adf7deb1ff6eef8e7763551
Admin@DESKTOP-EQ55Q8H <amina.hamzic@stu.ibu.edu.ba>
2024-01-11 20:10:33 +0100

    job-posting

--- freelance-platform/src/components/AddJobModal.jsx
+++ freelance-platform/src/components/AddJobModal.jsx
@@ -1,18 +1,45 @@
 // components/AddJobModal.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@material-ui/core';
+import { postJobMethod } from '../web3/contractInteraction';
+import initializeWeb3 from '../web3/initializeWeb3';
+
+
+
+
 
 const AddJobModal = ({ open, onClose }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [payment, setPayment] = useState('');
+  const [web3, setWeb3] = useState(null);
 
-  const handleSubmit = () => {
-    // Handle the submission logic here
-    console.log({ title, description, payment });
-    onClose(); // Close modal after submission
+
+  useEffect(() => {
+    const web3Instance = initializeWeb3();
+    setWeb3(web3Instance);
+  }, []);
+
+
+  const handleSubmit = async () => {
+    if (!title || !description || !payment) {
+      console.error("All fields are required");
+      return;
+    }
+    if (!web3) {
+      console.error('Web3 is not initialized');
+      return;
+    }
+ 
+    try {
+     
+      await postJobMethod(title, description, payment);
+      console.log("Job posted successfully");
+      onClose();
+    } catch (error) {
+      console.error("Error submitting job:", error);
+    }
   };
-
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Add a New Job</DialogTitle>
@@ -57,4 +84,8 @@ const AddJobModal = ({ open, onClose }) => {
   );
 };
 
+
 export default AddJobModal;
+
+
+
